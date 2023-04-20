dfb01ce286add58e6d568d76178629a05bc8db2f
David Carmo <davidsc2812@gmail.com>
2023-04-19 23:35:29 -0300

    feat: formatted the Date when user joined github

--- src/components/userInfo/userProfile/userProfile.component.tsx
+++ src/components/userInfo/userProfile/userProfile.component.tsx
@@ -3,6 +3,15 @@ import styles from "./userProfile.module.scss";
 import octocatImage from "../../../assets/images/octocat.svg";
 import { UserProfileProps } from "./userProfile.interface";
 export const UserProfile: React.FC<UserProfileProps> = ({ avatarURL, description, joined, name, username }) => {
+  const formatDate = (dateString: string) => {
+    const date = new Date(dateString);
+    const day = date.getDate();
+    const month = date.toLocaleString('default', { month: 'short'  }).replace('.', ' ');
+    const capitalizedMonth = month.charAt(0).toUpperCase() + month.slice(1);
+    const year = date.getFullYear();
+    return `${day} ${capitalizedMonth} ${year}`;
+  };
+  
   return (
     <div className={styles.container}>
       <div className={styles.userProfileInfoContainer}>
@@ -15,7 +24,7 @@ export const UserProfile: React.FC<UserProfileProps> = ({ avatarURL, description
             <a>{username ? `@${username}` : '---'}</a>
           </div>
           <div className={styles.joinedContainer}>
-            <p>{joined ? `Joined ${joined}` : null}</p>
+            <p>{joined ? `Joined ${formatDate(joined)}` : null}</p>
           </div>
         </div>
       </div>
